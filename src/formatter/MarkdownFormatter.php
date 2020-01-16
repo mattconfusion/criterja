48be7c5e728ca25d2553f0f04843a018e0580ae7
Matteo Radice <mattconfusion@hotmail.com>
2020-01-16 01:59:04 +0100

    test conversion to md ok.

--- src/formatter/MarkdownFormatter.php
+++ src/formatter/MarkdownFormatter.php
@@ -6,6 +6,7 @@ use Criterja\gherkin\Background;
 use Criterja\gherkin\Feature;
 use Criterja\gherkin\Scenario;
 use Criterja\gherkin\Table;
+use Criterja\gherkin\Step;
 
 class MarkdownFormatter implements Formatter {
     
@@ -18,45 +19,98 @@ class MarkdownFormatter implements Formatter {
     public function printFeature(Feature $feature): string
     {
         $featureLines = \array_map([$this, 'padText'], $feature->getDescription());
-
-        \array_unshift($featureLines, 'Feature: ' . $feature->getTitle());
-
+        $this->addSectionTitleToSectionLines($this->boldText('Feature'), $this->italicText($feature->getTitle()), $featureLines);
         return $this->renderAsMultiLineText($featureLines);
     }
 
+    /**
+     * Prints the Background section
+     *
+     * @param Background $background
+     * @return string
+     */
     public function printBackground(Background $background): string
     {
-        return '';
+        $steps = \array_map(function (Step $step) {
+            return $this->padText($this->formatStep($step));
+        }, $background->getSteps());
+
+        $this->addSectionTitleToSectionLines($this->boldText('Background'), $this->italictext($background->getTitle()), $steps);
+        return $this->renderAsMultiLineText($steps);
     }
 
+    /**
+     * Prints the formatted scenario (without examples)
+     *
+     * @param Scenario $scenario
+     * @return string
+     */
     public function printScenario(Scenario $scenario): string
     {
-        return '';
+        $steps = \array_map(function (Step $step) {
+            return $this->padText($this->formatStep($step));
+        }, $scenario->getSteps());
+
+        $this->addSectionTitleToSectionLines(
+            $this->boldText($scenario->getKeyword()),
+            $this->italicText($scenario->getTitle()),
+            $steps
+        );
+
+        return $this->renderAsMultiLineText($steps);
     }
 
     public function printExampleTable(Table $table): string
     {
-        return '';
+        $tHead = $this->padText($this->makeTableRow($table->getColumnsNames()));
+
+        $rows = \array_map(function (array $row) {
+            return $this->padtext($this->makeTableRow($row));
+        }, $table->getRows());
+        
+        $table = \array_merge([$tHead], $rows);
+        $this->addSectionTitleToSectionLines($this->boldText('Examples'), '', $table);
+        return $this->renderAsMultiLineText($table);
     }
 
     public function printSectionBreak(): string
     {
-        return '';
+        return "\r\n\r\n";
+    }
+
+    private function formatStep(Step $step): string
+    {
+        return $this->boldText($step->getKeyword()) . ' ' . $step->getCondition();
+    }
+
+    private function addSectionTitleToSectionLines(string $keyword, string $title, array &$sectionLines)
+    {
+        \array_unshift($sectionLines, $keyword .': ' . $title);
     }
 
     private function italicText(string $text): string
     {
-        return "*$text*";
+        return $text ? "*$text*" : '';
     }
 
     private function boldText(string $text): string
     {
-        return "**$text**";
+        return $text ? "**$text**" : '';
     }
 
     private function padText(string $text): string
     {
-        return "  $text";
+        return $text ? "  $text" : '';
+    }
+
+    private function makeTableCell(string $text): string
+    {
+        return $text ? "| $text |" : "| - |";
+    }
+
+    private function makeTableRow(array $rowValues): string
+    {
+        return "| " . \implode(" | ", $rowValues) . " | \r";
     }
 
     private function renderAsMultiLineText(array $strings): string
--- testmd.php
+++ testmd.php
@@ -5,6 +5,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 use Criterja\gherkin\AcceptanceCriteria;
 use Criterja\utils\FeatureFile;
 use Criterja\formatter\MarkdownFormatter;
+use Criterja\gherkin\Scenario;
 
 $file = new FeatureFile(__DIR__ . DIRECTORY_SEPARATOR . 'test.feature');
 
@@ -12,3 +13,17 @@ $ac = new AcceptanceCriteria($file);
 $mdf = new MarkdownFormatter();
 
 echo $mdf->printFeature($ac->getFeature());
+echo $mdf->printSectionBreak();
+echo $mdf->printBackground($ac->getBackground());
+echo $mdf->printSectionBreak();
+
+$scenarios = $ac->getScenarios();
+\array_walk($scenarios, function (Scenario $scenario) use ($mdf) {
+    echo $mdf->printScenario($scenario);
+    echo $mdf->printSectionBreak();
+
+    if ($scenario->hasExamples()) {
+        echo $mdf->printExampleTable($scenario->getExamples());
+        echo $mdf->printSectionBreak();
+    }
+});
